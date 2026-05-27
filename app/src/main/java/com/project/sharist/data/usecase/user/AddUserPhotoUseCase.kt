1c658e9aab4c69389eb2fe5fcf7f6bea5b73ca44
David Varela <david.varela@tecnico.ulisboa.pt>
2026-05-27 15:19:28 +0100

    Add photos to user and vehicle

---
+++ app/src/main/java/com/project/sharist/data/usecase/user/AddUserPhotoUseCase.kt
@@ -0,0 +1,4 @@
+package com.project.sharist.data.usecase.user
+
+class AddUserPhotoUseCase {
+}
---
+++ app/src/main/java/com/project/sharist/data/usecase/vehicle/AddVehiclePhotoUseCase.kt
@@ -0,0 +1,4 @@
+package com.project.sharist.data.usecase.vehicle
+
+class AddVehiclePhotoUseCase {
+}
